f29218573b8c9e86cdf73e5b20be87bee26f9036
abc351 <80473809+abc351@users.noreply.github.com>
2023-06-11 22:58:33 +0900

    Add files via upload

--- MFCApplication1/MFCApplication1/resource.h
+++ MFCApplication1/MFCApplication1/resource.h
@@ -15,6 +15,10 @@
 #define IDC_v1                          1005
 #define IDC_EDIT6                       1006
 #define IDC_v2                          1006
+#define IDC_v3                          1007
+#define IDC_start                       1008
+#define IDC_BUTTON3                     1009
+#define IDC_reset                       1009
 
 // Next default values for new objects
 // 
@@ -22,7 +26,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
