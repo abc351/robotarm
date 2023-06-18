6246bc318c9ab660721b01573e1665b85460c128
abc351 <80473809+abc351@users.noreply.github.com>
2023-06-18 09:07:34 +0100

    Add files via upload

--- pi/a.cpp
+++ pi/a.cpp
@@ -14,11 +14,11 @@
 using namespace std;
 pthread_t thr[3];
 char serial_name[100];
-
+char serial2_name[100];
 int sock0,sockw;
 int __port;
 void* network(void* dat);
-
+void* motion(void* dat);
 
 int memfd;
 volatile unsigned int *systimer, *CLO, *CHI,*gpio, *GPFSEL, *GPSET, *GPCLR, *GPLEV;
@@ -27,13 +27,13 @@ int main(int argc,char* argv[]){
 	//usleep(10000000u);
 	if(argc<3) return 0;
 	sprintf(serial_name,"/dev/%s",argv[1]);
+	sprintf(serial2_name,"/dev/%s",argv[3]);
 	__port=atoi(argv[2]);
 	cout<<serial_name<<endl;
 	cout<<"init"<<endl;
-	system("sudo motion\n");
-	cout<<"motion running"<<endl;
-	pthread_create(thr,NULL,network,reinterpret_cast<void*>(0));
 
+	pthread_create(thr,NULL,network,reinterpret_cast<void*>(0));
+	pthread_create(thr+1,NULL,motion,reinterpret_cast<void*>(0));
 	getchar();
 	cout<<"stop"<<endl;
 	close(sock0);
@@ -41,12 +41,21 @@ int main(int argc,char* argv[]){
 	system("sudo killall -9 motion\n");
 	cout<<"end"<<endl;
 }
+void* motion(void* dat){
+	system("sudo motion\n");
+	cout<<"motion running"<<endl;
+	return 0;
+}
 
 void* network(void* dat){
 	char sport[100];
-	termios otm,tm;
-	int port;
+	termios otm,tm,otm2,tm2;
+	int port,port2;
 	port=open(serial_name,O_RDWR|O_NOCTTY);
+	if(!port){
+		printf("port err\n");
+		return 0;
+	}
 	tcgetattr(port,&otm);
 	tcgetattr(port,&tm);
 	tm.c_iflag=IGNPAR;
@@ -55,6 +64,19 @@ void* network(void* dat){
 	tm.c_cflag=B9600|CS8|CLOCAL|CREAD;
 	tcsetattr(port,TCSAFLUSH,&tm);
 	tcflush(port,TCIFLUSH);
+	port2=open(serial2_name,O_RDWR|O_NOCTTY);
+	if(!port2){
+		printf("port2 err\n");
+		//return 0;
+	}
+	tcgetattr(port2,&otm2);
+	tcgetattr(port2,&tm2);
+	tm2.c_iflag=IGNPAR;
+	tm2.c_oflag=0;
+	tm2.c_lflag=0;
+	tm2.c_cflag=B9600|CS8|CLOCAL|CREAD;
+	tcsetattr(port2,TCSAFLUSH,&tm2);
+	tcflush(port2,TCIFLUSH);
 	char recv[10240];
 	const char html_upp[]=
 	"HTTP/1.0 200 OK\n"\
@@ -103,6 +125,7 @@ void* network(void* dat){
 		if(strstr(recv,"/arm")!=0){
 			char* t;
 			double x,y,z,th;
+			int m1,m2;
 			int g;
 			
 			t=strstr(recv,"x=");
@@ -117,7 +140,16 @@ void* network(void* dat){
 			if(t!=0) g=atoi(t+2);
 			sprintf(html,"%s\nx=%f y=%f z=%f t=%f g=%d\n%s",html_upp,x,y,z,th,g,html_low);
 			int len=sprintf(sport,"%f %f %f %f %d\n",x,y,z,th,g);
+			printf("%s\n",sport);
 			write(port,sport,len);
+			t=strstr(recv,"m1=");
+			if(t!=0) m1=atoi(t+3);
+			
+			t=strstr(recv,"m2=");
+			if(t!=0) m2=atoi(t+3);
+			len=sprintf(sport,"A%d %d",m1,m2);
+			write(port2,sport,strlen(sport));
+			
 		}	
 		else{
 			sprintf(html,"%s\n%s",html_upp,html_low);
@@ -128,6 +160,7 @@ void* network(void* dat){
 	}
 	close(sock0);
 	cout<<"sock closed"<<endl;
-	tcsetattr(port,TCSAFLUSH,&otm);
+	tcsetattr(port, TCSAFLUSH,&otm);
+	tcsetattr(port2,TCSAFLUSH,&otm2);
 	return 0;
 }
